00be9fd12dbfa53e627ccb5f633225ac50dd5480
Ryxwaer <ryxwaer@gmail.com>
2024-08-06 23:00:45 +0200

    fixed wrong data being pulled by api

--- app/scraper.py
+++ app/scraper.py
@@ -23,7 +23,12 @@ def extract_urls(id, estate):
 
 
 def fetch_new_listings(url: str) -> List[dict]:
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json()
     # throw error
