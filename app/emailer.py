1d6cf6873f63a5217879f45d697efe7bf4b1a860
Ryxwaer <ryxwaer@gmail.com>
2024-08-07 14:18:01 +0200

    mail body has html formatting now

--- app/emailer.py
+++ app/emailer.py
@@ -12,10 +12,25 @@ EMAIL_USER = os.getenv("EMAIL_USER")
 EMAIL_PASS = os.getenv("EMAIL_PASS")
 
 
+def compose_email_body(new_listings):
+    formatted_listings = "".join(
+        [f"<a href='{listing['url']}' style='text-decoration: none; color: inherit;' target='_blank'>"
+         f"<div style='margin-bottom: 20px; padding: 20px; border: 1px solid #e0e0e0; border-radius: 8px; box-shadow: "
+         f"0 2px 8px rgba(0,0,0,0.1); transition: box-shadow 0.3s ease-in-out;'>"
+         f"<h2 style='margin: 0 0 10px; font-size: 1.5em; color: #2c3e50;'>{listing['name']}</h2>"
+         f"<p style='margin: 10px 0; color: #27ae60; font-weight: bold; font-size: 1.2em;'>{listing['price']} CZK</p>"
+         f"<p style='margin: 5px 0; font-size: 1em; color: #555;'><strong>Locality:</strong> {listing['locality']}</p>"
+         f"<p style='margin: 5px 0; font-size: 1em; color: #555;'><strong>Features:</strong> {listing['features']}</p>"
+         f"</div>"
+         f"</a>"
+         for _, listing in new_listings.iterrows()]
+    )
+    return f"<html><body style='font-family: Arial, sans-serif; line-height: 1.6;'>{formatted_listings}</body></html>"
+
+
 class Emailer:
     def __init__(self):
         self.transporter = None
-        self.connect()
 
     def connect(self):
         try:
@@ -45,10 +60,7 @@ class Emailer:
 
         recipients = config["recipients"].split(',')
         subject = f"SReality: nove inzeraty - {config['subject']}"
-        message = "Nove inzeraty:\n\n" + "\n\n".join(
-            [f"{listing['name']} : {listing['price']} CZK\n{listing['url']}\n - {listing['locality']} {listing['features']}]\n"
-             for _, listing in new_listings.iterrows()]
-        )
+        message = compose_email_body(new_listings)
 
         try:
             msg = MIMEMultipart()
@@ -56,7 +68,7 @@ class Emailer:
             msg['To'] = ", ".join(recipients)
             msg['Subject'] = subject
 
-            msg.attach(MIMEText(message, 'plain'))
+            msg.attach(MIMEText(message, 'html'))
 
             self.transporter.sendmail(EMAIL_USER, recipients, msg.as_string())
             logger.debug("Email sent successfully")
